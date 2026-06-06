07d38f1868d93f402e04df5d502dbfde26b658fd
z80705682z <z80705682z42@gmail.com>
2026-06-06 12:23:29 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // 或者原本的引入方式，Vite 預期它是這樣寫的
+import tailwindcss from 'tailwindcss' 
 
 // https://vitejs.dev/config/
 export default defineConfig({
