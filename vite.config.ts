eecf1d903da259429adcbfadd9e2e5a7bb240b40
z80705682z <z80705682z42@gmail.com>
2026-06-06 13:12:20 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss' 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/presentation/',
-  plugins: [
-    react(),
-    tailwindcss()
-  ]
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
 })
